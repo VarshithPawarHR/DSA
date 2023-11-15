7dddd4d7f8d067523fa1c10dcf6b39b04e90628d
Varshith Pawar H R <143952673+VarshithPawarHR@users.noreply.github.com>
2023-11-15 22:17:11 +0530

    circularqueue.c

--- circularqueue.c
+++ circularqueue.c
@@ -2,88 +2,73 @@
 #include <stdlib.h>
 #define max 5
 
-struct queue
-{
-    int rear;
-    int front;
-    int count;
-    int q[max];
-}s;
-int main()
-{
-    int choice,value;
-    s.rear=-1;
-    s.front=0;
-    s.count=0;
-    while(1)
-    {
-        printf("enter your choice \n");
-        printf("1.insert \n 2.delete \n 3.display \n 4.exit \n");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-        case 1:
-            printf("enter the value you want to insert");
-            scanf("%d", &value);
-            insert(&s,value);
-            break;
-        case 2:
-            delete(&s);
-        case 3:
-            display(&s);
-         case 4:
-             exit(0);
-        }
-    }
-    return 0;
+int rear = -1;
+int front = 0;
+int a[max];
+int count = 0;
+int i;
 
-}
-
-void insert(struct queue *s, int value)
+void insert(int value)
 {
-    if(s->count ==  max)
+    if(count ==max)
     {
         printf("queue is full");
     }
     else
     {
-
-        s->rear = (s->rear+1)%max;
-        s->q[s->rear] = value;
-        (s->count)++;
+        rear = rear +1%max;
+        count++;
+        a[rear] = value;
+        printf("insertion is successful");
 
     }
 }
-void delete(struct queue *s)
+void delete()
 {
-    if(s->count==0)
+    if(count == 0)
     {
-        printf("queue is empty");
+        printf("queue is empty nothing to delete");
     }
     else
     {
-        (s->count)--;
-        s->front= (s->front +1)%max;
-        printf("deleted e;lement is %d", s->q[s->front]);
-       
+        count--;
+        front = front + 1%max;
+        printf("deleted value is %d", a[front]);
+
+
     }
 }
-void display(struct queue *s)
-{
-    if (s->count == 0)
-    {
-        printf("queue is empty");
+void display() {
+    if(count == 0) {
+        printf("queue is empty\n");
     }
-    else
-    {
-        printf("contents of queue are: ");
-
-        for (int i = 0; i <s->count; i++)
-        {
-            int x= s->q[s->front];
-            s->front = (s->front + 1) % max;
-            printf("%d ", x);
+    else {
+        int j = front;
+        for (i = 0; i < count; i++) {
+            printf("%d ", a[j]);
+            j = (j + 1) % max; // Move to the next element in the circular queue
         }
-        printf("\nnumber of elements are: %d\n", s->count);
+        printf("\n");
     }
+}
+
+
+int main()
+{
+    int choice,value;
+    while(1)
+    {
+        printf("enter the choice \n");
+        printf("1.insert \n2.delete \n3.display \n4.exit \n");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+            case 1 : printf("enter the value you want to insert");scanf("%d",&value);insert(value);break;
+            case 2: delete(); break;
+            case 3:display(); break;
+            case 4: exit(0);
+            default : printf("invalid bsdk");
+        }
+    }
+    return 0;
 }
