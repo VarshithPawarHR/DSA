6bf51d5f688a8ea1a7524721ffe7b8f58bba94dd
VarshithPawarHR <varshithpawarhr@gmail.com>
2023-12-27 14:04:30 +0530

    commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,43 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int [] a ={2,-6,77,88,-9,-11,55};
+
+        for( int unsortedindex = a.length-1;unsortedindex>0;unsortedindex--)
+        {
+            for(int i=0;i<unsortedindex;i++)
+            {
+                if(a[i]>a[i+1]) //decreasinhg if(a[i]<a[i+1])
+                {
+                    swap(a, i,i+1);
+                }
+            }
+        }
+
+        for (int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+
+
+
+
+
+
+
+
+    }
+    public static void swap(int [] a, int i, int j)
+    {
+        if(i==j)
+        {
+            return;
+        }
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+
+
+}
